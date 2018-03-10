09ab667f634fc704675500ec4f75e7cddfbb060e
Peter Zatka-Haas <peter@peterzh.com>
2018-03-09 21:51:19 -0700

    Moved compiled files to bin directory

--- compileStanModel.m
+++ compileStanModel.m
@@ -1,5 +1,17 @@
 function compileStanModel(stanFile)
-    sm = StanModel('file',stanFile);
+    baseDir = fileparts(mfilename('fullpath'));
+    binDir = fullfile(baseDir,'bin');
+    if ~exist(binDir,'dir')
+        mkdir(binDir);
+    end
+    
+    %Copy stanFile to binary directory
+    copyfile(fullfile(baseDir,stanFile), fullfile(binDir,stanFile));
+    
+    %Compile model
+    sm = StanModel('file',fullfile(binDir,stanFile));
     sm.compile();
+    
+    %Save copy of stanmodel object
     save(sm.model_name,'sm');
 end
