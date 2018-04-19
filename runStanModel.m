3bfebcf71b5be358d7b0e8d85167bff291b04e8d
Peter Zatka-Haas <p.zatka@ucl.ac.uk>
2018-04-19 14:49:07 +0100

    cleaned up

--- runStanModel.m
+++ runStanModel.m
@@ -14,7 +14,7 @@ data = struct('contrastLeft', contrast_left,...
               'sessionID', sessionID,...
               'subjectID', subjID);
           
-data2 = getrow(data, data.subjectID==12);
+data2 = getrow(data, data.subjectID==3);
 [~,~,data2.sessionID]=unique(data2.sessionID);
 [~,~,data2.subjectID]=unique(data2.subjectID);
           
@@ -32,8 +32,28 @@ data = struct('contrastLeft', contrast_left,...
               'choice', response,...
               'sessionID', sessionID,...
               'subjectID', subjID);
+data2 = getrow(data, data.subjectID==1);
+[~,~,data2.sessionID]=unique(data2.sessionID);
+[~,~,data2.subjectID]=unique(data2.subjectID);
 
+
+%% Simulate data
+N=10000;
+bias=2;
+sens=2.5;
+cDiff = repmat([-1;-0.25;0;0.25;1],N/5,1);
+z = bias+sens*cDiff;
+pR = exp(z)./(1+exp(z));
+
+
+data3 = struct('contrastLeft', (cDiff<0).*(-cDiff),...
+              'contrastRight', (cDiff>0).*cDiff,...
+              'choiceR', binornd(1,pR),...
+              'sessionID', ones(N,1),...
+              'subjectID', ones(N,1));
+          
 %% Load model
-b = behavModel(data,'bias_sens');
+b = behavModel(data2,'bias1_sensL_sensR_Exponent');
 b.getPosterior;
 b.plotPosteriorMarginals;
+b.plotPsych;
